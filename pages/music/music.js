2ace98e65008302d8b9daaaec086b3e48af938a1
小胖子 <ly1989124423@163.com>
2017-04-18 23:27:28 +0800

    修改book部分样式

--- pages/music/music.js
+++ pages/music/music.js
@@ -12,10 +12,10 @@ Page({
       {photo:"http://p4.music.126.net/JaYWNg5HQiSYyHF3bfrATQ==/5751545324970007.jpg",song: "红玫瑰",author:"陈奕迅",word: "得不到的永远在骚动，被偏爱的都有恃无恐，玫瑰的红容易受伤的梦，握在手中却流失于指缝，又落空。"},
       {photo:"http://img.zcool.cn/community/01fe4d575a31f50000018c1b7cf982.jpg@900w_1l_2o_100sh.jpg",song: "好久不见",author:"陈奕迅",word: "我多么想和你见一面 看看你最近改变 不再去说从前 只是寒暄 对你说一句 只是说一句 好久不见"},
       {photo:"http://www.yoka.com/dna/pics/Star/ba151b1a/d35e53abed111ad9ea.jpg",song: "陪安东尼度过漫长岁月",author:"陈奕迅",word: "让我再陪你一段，陪你把沿路感想活出了答案，陪你把独自孤单变成了勇敢。"},
-      {photo:"http://img4.duitang.com/uploads/item/201304/24/20130424133632_cuvmQ.thumb.600_0.jpeg",song: "Shall We Talk",author:"陈奕迅",word: "成人只寄望收获 情人只听见承诺 为何都不大懂得努力珍惜对方"},
+      {photo:"http://imgcache.cjmx.com/music/201610/20161004154856569.jpg",song: "Shall We Talk",author:"陈奕迅",word: "成人只寄望收获 情人只听见承诺 为何都不大懂得努力珍惜对方"},
       {photo:"http://mingxing.facang.com/uploads/allimg/151222/111H53354-0.jpg",song: "斗战神",author:"陈奕迅",word: "不做随风飘的沙，不做秋叶上蚂蚱，不坐待冬雪融化，敢问谁敢栽我生死造化。"},
       {photo:"http://aliimg.changba.com/cache/photo/147758921_640_640.jpg",song: "不要说话",author:"陈奕迅",word: "愿意用一支黑色的铅笔，画一出沉默舞台剧。灯光再亮，也抱住你。愿意，在角落唱沙哑的歌。再大声也都是给你。"},
-      {photo:"http://imgcache.cjmx.com/music/201610/20161004154856569.jpg",song: "风继续吹",author:"张国荣",word: "风继续吹不忍远离 心里亦有泪不愿流泪望着你 过去多少快乐记忆何妨与你一起去追 要将忧郁苦痛洗去 柔情蜜意我愿记取."}
+      {photo:"http://gb.cri.cn/mmsource/images/2012/04/26/68/18021298023368227896.jpg",song: "风继续吹",author:"张国荣",word: "风继续吹不忍远离 心里亦有泪不愿流泪望着你 过去多少快乐记忆何妨与你一起去追 要将忧郁苦痛洗去 柔情蜜意我愿记取."}
       ]
   },
   onLoad: function () {

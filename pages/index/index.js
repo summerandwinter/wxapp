2bf57b79d84c9ed923726c12954cd728807342c5
小胖子 <ly1989124423@163.com>
2017-06-29 07:17:33 +0800

    修改首页

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,5 +1,8 @@
 //explore.js
 //获取应用实例
+var util = require('../../utils/util.js');
+const AV = require('../../utils/av-weapp-min.js');
+const Card = require('../../model/card');
 var app = getApp()
 Page({
   data: {
@@ -52,57 +55,37 @@ Page({
   },
   initData: function () {
     var that = this;
-    var result = {
-      "data": [
-        {
-          "img_url": "http://7xqnv7.com2.z0.glb.qiniucdn.com/usersharemovie_1464188073",
-          "shares": 270,
-          "user": {
-            "city": "Nanjing",
-            "id": "590be679ac502e006cdc63c0",
-            "nickName": "一言",
-            "gender": 1,
-            "province": "Jiangsu",
-            "avatarUrl": "http://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTI6ra6icibeicqZcnZTtxhWRd2SlgeDzYCfJoXvhveRDq2RsTia0wfBxicYQ5ZaS4r2WYAKxDYmVvn7nNA/0"
-          },
-          "likes": 1038,
-          "content": "因为你，我想要变成一个更好的人，不想成为你的负担，因此发奋，只是想证明我足以与你相配。",
-          "name": "侧耳倾听",
-          "time": "2016年6月1日",
-          "id": "593947698d6d810058503830"
-        },
-        {
-          "img_url": "http://7xqnv7.com2.z0.glb.qiniucdn.com/usersharemovie_1464082504",
-          "shares": 280,
-          "user": {
-            "city": "Nanjing",
-            "id": "590be679ac502e006cdc63c0",
-            "nickName": "一言",
-            "gender": 1,
-            "province": "Jiangsu",
-            "avatarUrl": "http://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTI6ra6icibeicqZcnZTtxhWRd2SlgeDzYCfJoXvhveRDq2RsTia0wfBxicYQ5ZaS4r2WYAKxDYmVvn7nNA/0"
-          },
-          "likes": 897,
-          "content": "爱，不是寻找一个完美的人，而是学会用完美的眼光，欣赏那个并不完美的人。\n\n因为爱你，只要你一个肯定，我就足够勇敢。",
-          "name": "哈尔的移动城堡",
-          "time": "3天前",
-          "id": "593351b361ff4b389e37087e"
-        }
-      ],
-      "hasMore": true,
-      "code": 200,
-      "count": 900,
-      "page": 1
+    var initParam = {
+      loading: {
+        hidden: false
+      },
+      info: {
+        list: [],
+        hasMore: true,
+        hasRefesh: true,
+        page: 1,
+        hidden: true
+      },
+      isLoading: false
     }
-    if (result.code == 200) {
-      that.setData({
-        'info.page': 1,
-        'info.hasMore': result.hasMore,
-        'loading.hidden': true,
-        'info.list': that.data.info.list.concat(result.data),
-        'info.hidden': false
-      })
-    }
+    that.setData(initParam);
+    var page = that.data.info.page;
+    var data = { 'page': page }
+    AV.Cloud.run('selection', data).then(function (result) {
+      // 调用成功，得到成功的应答 data
+      console.log(result)
+      if (result.code == 200) {
+        that.setData({
+          'info.page': page + 1,
+          'info.hasMore': result.hasMore,
+          'loading.hidden': true,
+          'info.list': that.data.info.list.concat(result.data),
+          'info.hidden': false
+        })
+      }
+    }, function (err) {
+      // 处理调用失败
+    });
   },
   onLoad: function () {
     console.log('生命周期:explore-load')

b301eb3fb5ecfe830e0f9542867464142d31527b
小胖子 <ly1989124423@163.com>
2017-07-26 07:14:34 +0800

    fix something

--- pages/search/index/index.js
+++ pages/search/index/index.js
@@ -39,13 +39,37 @@ Page({
       url: '/pages/card/card'
     })
   },
-  initData: function () {
+  initData2: function () {
     var that = this;
-
+    var cates = [{ 'title': '「 电影 」', 'description': '光影之间 品味感动', 'event': 'toMovie' },
+      { 'title': '「 音乐 」', 'description': '旋律背后的文字最能打动人心', 'event': 'toMusic' },
+      { 'title': '「 读书 」', 'description': '于墨香之中宁静心神', 'event': 'toBook' },
+      { 'title': '「 独白 」', 'description': '总有那么一句话让你念念不忘', 'event': 'toWord' }]
+    that.setData({
+      'loading.hidden': true,
+      'info.list': cates,
+      'info.hidden': false
+    })
+  },
+  initData:function(){
+    var that = this;
+    AV.Cloud.run('cates').then(function (result) {
+      console.log(result)
+      if (result.code == 200) {
+        that.setData({
+          'loading.hidden': true,
+          'info.list': result.data,
+          'info.hidden': false
+        })
+      }
+    }, function (err) {
+      console.log(err.code)
+    });
   },
   onLoad: function () {
     console.log('生命周期:card-load')
     var that = this;
+    that.initData();
     util.showShareMenu();
   },
   onReady: function () {
